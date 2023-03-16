1c98a3da684fd7a817344b2d235eced6e5c3c705
Matkip0 <biegel987@gmail.com>
2023-03-16 13:43:30 +0100

    Add project files.

---
+++ Banksystem/BankAccount.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banksystem
+{
+    public class BankAccount
+    {
+        double balance = 100;
+        public BankAccount()
+        {
+
+        }
+
+        public void GetBalance()
+        {
+            Console.WriteLine($"Du har {balance}kr. på din konto");
+        }
+
+        public void Deposit()
+        {
+
+
+            Console.WriteLine("Indtast hvor mange penge du vil sætte ind");
+            if (double.TryParse(Console.ReadLine(), out double amount))
+            {
+                balance += amount;
+                Console.WriteLine($"Du har lagt {amount}kr. ind på din konto");
+                Console.WriteLine($"Du har nu {balance}kr. på din konto");
+            }
+            else
+            {
+                Console.WriteLine("Der skete en fejl");
+            }
+
+        }
+        public void Withdraw()
+        {
+            if (double.TryParse(Console.ReadLine(), out double amount))
+            {
+                if (amount <= balance)
+                {
+                    balance -= amount;
+                    Console.WriteLine($"Du har trukket {amount}kr. ud af din konto");
+                    Console.WriteLine($"Du har nu {balance}kr. på din konto");
+                }
+                else
+                {
+                    Console.WriteLine($"Kunne ikke gennemføre handlingene fordi du ikke har nok penge");
+                    GetBalance();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Der skete en fejl");
+            }
+        }
+    }
+}
---
+++ Banksystem/Program.cs
@@ -0,0 +1,37 @@
+﻿using Banksystem;
+
+BankAccount user = new BankAccount();
+bool repeat = true;
+
+while (repeat == true)
+{
+    Console.WriteLine("""
+        Velkommen til din bank konto
+        Hvad vil du gøre?
+        1. Se hvor mange penge der er på kontoen 
+        2. sæt penge ind på kontoen
+        3. tag penge ud af kontoen
+        """);
+    if(int.TryParse(Console.ReadLine(), out int valg))
+    {
+        switch (valg)
+        {
+            case 0:
+                repeat = false;
+                break;
+
+            case 1:
+                user.GetBalance();
+                break;
+
+            case 2:
+                user.Deposit();
+                break;
+
+            case 3:
+                user.Withdraw();
+                break;
+
+        }
+    }
+}
